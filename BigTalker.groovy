cacc30865b7166460b4ac62c68c807cebf83683e
Brian Lowrance <brian@rayzurbock.com>
2016-01-17 18:52:46 -0600

    Release 1.1.4-Beta8
    **Modify Feature:** Add Lannouncer into the supported device descriptions for speechSynthesis.
    **Modify Feature:** Remove unsupported Volume setting / function when operating in speechSynthesis mode.
    **New Feature:** Add adjustWeatherPhrase() function to convert "xxF" to "xx degrees Fahrenheit", mph to "Miles Per Hour", NNE to "North Northeast", etc..
    **BugFix:** Replace special characters found in phrase (ie: URL tokens) such as those that end up in weather reports which cause Lannouncer to not speak when expected.
    **BugFix:** Fix Talk() routine for speechSynthesis only devices which typically do TTS on the phrase passed to them on their own or within their deviceType.
    **Metadata Update: Adjusted copyright from 2014-2015 to 2014-2016.

--- BigTalker.groovy
+++ BigTalker.groovy
@@ -1,6 +1,6 @@
 /**
  *  BIG TALKER -- Version 1.1.4-Beta8 -- A SmartApp for SmartThings Home Automation System
- *  Copyright 2014-2015 - rayzur@rayzurbock.com - Brian S. Lowrance
+ *  Copyright 2014-2016 - rayzur@rayzurbock.com - Brian S. Lowrance
  *  For the latest version, development and test releases visit http://www.github.com/rayzurbock
  *
  *  This SmartApp is free. Donations to support development efforts are accepted via: 
